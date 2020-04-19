7f732b68731d9d74c6eda7bec122c85b1e84657b
Faridshd <faridshahdad17@gmail.com>
2020-04-19 17:52:20 +0430

    Updates

---
+++ precache-manifest.e294ba8b2096b87d508c3e74733c348e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0fb213cffb88f9ba96d515dbde91e5",
+    "url": "/Faridshd-Maktab/index.html"
+  },
+  {
+    "revision": "ac6ffaf4a3d8bb5154db",
+    "url": "/Faridshd-Maktab/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f04129f0248b4219842e",
+    "url": "/Faridshd-Maktab/static/css/main.9f6e172e.chunk.css"
+  },
+  {
+    "revision": "ac6ffaf4a3d8bb5154db",
+    "url": "/Faridshd-Maktab/static/js/2.bd76bcf9.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "/Faridshd-Maktab/static/js/2.bd76bcf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04129f0248b4219842e",
+    "url": "/Faridshd-Maktab/static/js/main.71408c98.chunk.js"
+  },
+  {
+    "revision": "b023308c99430d390db5",
+    "url": "/Faridshd-Maktab/static/js/runtime-main.ec9ee612.js"
+  },
+  {
+    "revision": "13321fe8c144ffd100cc06efbd1e4425",
+    "url": "/Faridshd-Maktab/static/media/About.13321fe8.jpg"
+  },
+  {
+    "revision": "50953027678f2e493ea34b63733f26cd",
+    "url": "/Faridshd-Maktab/static/media/HomeTitle.50953027.png"
+  },
+  {
+    "revision": "aa5ce3a07197b8376e09ea44c60f6ae1",
+    "url": "/Faridshd-Maktab/static/media/Programming.aa5ce3a0.jpg"
+  }
+]);
